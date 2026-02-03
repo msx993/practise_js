234edcfdf9b92ce4ac355e1b2831cd891407123a
msx993 <mivanovych2012@gmail.com>
2026-02-03 19:23:46 +0200

    first  lesson js

---
+++ js/01-variables.js
@@ -0,0 +1,19 @@
+const myName = "max";
+console.log(myName)
+
+const myAge = 13;
+console.log(myAge)
+
+console.log(typeof myName);
+
+console.log(typeof myAge);
+
+
+const productName = "cup";
+console.log(productName)
+
+let productPrice = 5.99;
+console.log(productPrice)
+
+productPrice = 10;
+console.log(productPrice)
