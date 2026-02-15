eb6d80a8cbbbf2c29bd6b165b308a6ede911baf2
msx993 <mivanovych2012@gmail.com>
2026-02-15 19:38:07 +0200

    дз 1

--- js/01-variables.js
+++ js/01-variables.js
@@ -1,19 +1,31 @@
 const myName = "max";
 console.log(myName)
 
+console.log(typeof myName);
+
 const myAge = 13;
 console.log(myAge)
 
-console.log(typeof myName);
-
 console.log(typeof myAge);
 
+const isStudent = true;
 
-const productName = "cup";
-console.log(productName)
+console.log(typeof isStudent);
 
-let productPrice = 5.99;
-console.log(productPrice)
+const myString = "п'яний чоловік не падає він атакує підлогу";
+console.log(myString)
 
-productPrice = 10;
-console.log(productPrice)
+const myNumber = 25;
+console.log(typeof myNumber);
+
+const isComing = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(typeof isComing);
+
+const userName = prompt("Введіть своє ім’я");
+console.log(typeof userName);
+
+const isAlert = alert("Увага! Дія може бути небезпечною.");
+console.log(typeof isComing);
+
+const isComfirm = confirm("Ви впевнені, що хочете продовжити?")
+console.log(typeof isComing);
