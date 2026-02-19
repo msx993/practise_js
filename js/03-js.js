16f33e68c50ce5b50d6b91fa61f23d759cfaf457
msx993 <mivanovych2012@gmail.com>
2026-02-19 18:28:44 +0200

    п

---
+++ js/03-js.js
@@ -0,0 +1,7 @@
+// Welcome guestName, your room number is roomNumber!
+const guestName = "Mango";
+const roomNumber = 207;
+const greeting = "Welcome" + " " + guestName + ", your room number is" + roomNumber;
+const greeting2 =`Welcome &{guestName}, your room number is &{roomNumber}!`;
+console.log(greeting);
+// console.log(greeting2);
