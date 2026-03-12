de90d43b4bbcea7aed8926e4e8579ea5c4778eab
msx993 <mivanovych2012@gmail.com>
2026-03-12 18:17:23 +0200

    я

---
+++ js/loop.js
@@ -0,0 +1,21 @@
+
+let number = 10
+
+
+
+// while (number > 1 ) {
+//  console.log(number);
+// number -=1
+// }
+
+// let count1 = 0;
+
+//  do {
+//    console.log(`Count: ${count1}`);
+//    count1 += 1;
+//  } while (count1 <= 10);
+
+
+// for (let i = 0; i <= 10; i += 1) {
+//     console.log(i); 
+// }
