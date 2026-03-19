0c285a062fd597b127963aa4ad0cb8432bbcefd9
msx993 <mivanovych2012@gmail.com>
2026-03-19 18:35:30 +0200

    s

---
+++ js/arrays.js
@@ -0,0 +1,19 @@
+// const numbers = [0, 1, 2, 3, 4]
+// // console.log(numbers.length);
+// // console.log(numbers.length-1);
+// // console.log(numbers[numbers.length-1]);
+// // console.log(numbers[0]);
+
+
+
+const fruits = ["apple", "banana", "pear", "peach"];
+// console.log(fruits.length);
+// console.log(fruits[fruits.length-1]);
+// console.log(fruits[0]);
+
+fruits[2] = 'mango'
+
+for(let i = 0; i < fruits.length; i += 1){
+console.log(fruits[i]);
+
+}
