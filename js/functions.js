782e7f3c3fad78b40cb9d484c47f0a2173980e57
msx993 <mivanovych2012@gmail.com>
2026-04-07 19:31:16 +0300

    урок 9

--- js/functions.js
+++ js/functions.js
@@ -88,3 +88,50 @@ taxRate - число, податкова ставка. Необов'язкови
 //     return "Sorry! We are out of stock!"
 // }
 // checkStorage(["apple", "plum", "pear"], "pLuM")
+
+
+
+
+// створити функцію для створення настроюваного цикла
+// з параметрами від до та кроком
+// // count(1, 5); // From = 1, To = 5, step = 1
+// // count(2); // From = 2, To = 10, step = 1
+// // count(undefined, 5, 2); // From = 0, To = 5, step = 2
+// // // count(); // From = 0, To = 10, step = 1
+
+
+// function count (from = 1, to = 10, step = 1) {
+// for (let i = from; i <=to; i += step) {
+//     console.log(i); 
+// }
+// }
+
+// // count()
+// count(5, 10, 2.5)
+// // count(1, 5, 1)
+// // count(2, 10, 1)
+// // count(0, 5, 2)
+// // count(0, 10, 1)
+
+
+
+
+
+
+const myScore = function (score) {
+  if ( score < 100 && score> 91) {
+console.log("A");
+    } else   if ( score < 90 && score > 81) {
+console.log("B");
+    } else   if ( score < 80 && score > 71) {
+console.log("C");
+    } else   if ( score < 70 && score > 0) {
+console.log("D");
+    } else {
+        console.log("Введіть коректне значення.");
+        
+    }
+}
+myScore(95)
+
+
