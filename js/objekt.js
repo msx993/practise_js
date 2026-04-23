da2456454e36bf11faa8001fdeb617a59333615f
msx993 <mivanovych2012@gmail.com>
2026-04-23 18:29:17 +0300

    .

---
+++ js/objekt.js
@@ -0,0 +1,29 @@
+const apartment = {
+descr: "Spacious apartment in the city center",
+rating: 4,
+price: 2153,
+tags: ["premium", "promoted", "top"],
+}
+
+apartment.ownerName = "Henry"
+apartment.ownerPhone = "982-126-1588"
+apartment.ownerEmail = "henry.carter@aptmail.com"
+apartment.tags.push("trusted")
+// descr - рядок, що містить опис, 
+// значення "Spacious apartment in the city center";
+// rating - число, що містить рейтинг, значення 4;
+// price - число, що містить ціну, значення 2153;
+// tags - масив рядків, що містить метаінформацію,
+//  значення ["premium", "promoted", "top"].
+
+
+
+// 2. 
+// Доповни об'єкт квартири властивістю owner, 
+// значенням якого буде об'єкт з інформацією про власника.
+
+// Додай об'єкту owner наступні властивості:
+
+// name - рядок, що містить ім'я власника, значення "Henry";
+// phone - рядок, що містить номер телефону, значення "982-126-1588";
+// email - рядок, що містить пошту, значення "henry.carter@aptmail.com"
