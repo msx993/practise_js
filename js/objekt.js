98b59230f60cabe34122849084ea8809785019b5
msx993 <mivanovych2012@gmail.com>
2026-04-26 12:01:58 +0300

    .

--- js/objekt.js
+++ js/objekt.js
@@ -1,29 +1,81 @@
+
+
+
+const area = 60
+const rooms = 3
+const key = "location"
+
+
 const apartment = {
 descr: "Spacious apartment in the city center",
 rating: 4,
 price: 2153,
 tags: ["premium", "promoted", "top"],
+area,
+rooms,
+[key]: {
+    country: "Spain",
+    city: "Majorka",
+},
+
+changePrice(newPrise) {
+this.prise = 5034
+},
+
+
+showDescr() {
+    console.log(this.descr);
+    
+},
+
+
+showRating() {
+    return this.rating
+},
+
+addTag(newTag) {
+this.tags.push()
+}
 }
 
+apartment.changePrice(50000)
+apartment.newTag("city centre")
+apartment.showDescr();
+apartment.owner = {};
+const owmerName = "Genry"
+console.log(apartment);
+apartment.owner.name = "Max";
+apartment.price = 75000;
+apartment.rating = 5;
+apartment.tags.push("trusted")
+
+
+
+
+
+
 apartment.ownerName = "Henry"
-apartment.ownerPhone = "982-126-1588"
+apartment.ownerPhone = "892-167-1488"
 apartment.ownerEmail = "henry.carter@aptmail.com"
 apartment.tags.push("trusted")
-// descr - рядок, що містить опис, 
-// значення "Spacious apartment in the city center";
-// rating - число, що містить рейтинг, значення 4;
-// price - число, що містить ціну, значення 2153;
-// tags - масив рядків, що містить метаінформацію,
-//  значення ["premium", "promoted", "top"].
 
 
 
-// 2. 
-// Доповни об'єкт квартири властивістю owner, 
-// значенням якого буде об'єкт з інформацією про власника.
 
-// Додай об'єкту owner наступні властивості:
 
-// name - рядок, що містить ім'я власника, значення "Henry";
-// phone - рядок, що містить номер телефону, значення "982-126-1588";
-// email - рядок, що містить пошту, значення "henry.carter@aptmail.com"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
